22a011030be8bc991999ec67e9af0ee1cde6110c
Jeong Shin <72616591+jshinnn@users.noreply.github.com>
2023-05-24 18:12:58 -0700

    wooooo

--- level.gd
+++ level.gd
@@ -4,7 +4,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$OldDaikon/Hint.visible = false
+	$Mechanic/Hint.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,12 +17,12 @@ func _on_music_finished():
 
 
 func _on_old_daikon_body_exited(body):
-	$OldDaikon/Hint.visible = false
+	$Mechanic/Hint.visible = false
 
 
 func _on_old_daikon_body_entered(body):
 	if body.name == "Daikon":
-		$OldDaikon/Hint.visible = true
+		$Mechanic/Hint.visible = true
 
 
 
